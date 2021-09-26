e64920e55346ebc6e8a2c5baa1a20e1cee8bd3bb
LucasFDutra <lucasfelipedutra@gmail.com>
2021-09-26 15:46:45 -0300

    receber arquivo de configurações externamente config.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+// import configFile from '../config.json'
+const API_URL = window.API_URL;
 
 const api = axios.create({
-    baseURL: 'http://0.0.0.0:5000'
+    baseURL: API_URL
 })
 
 export {api}
