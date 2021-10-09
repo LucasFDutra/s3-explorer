f2e3849c278399a72f80b9e83a9827e977397e2f
LucasFDutra <lucasfelipedutra@gmail.com>
2021-10-09 14:10:32 -0300

    escondendo a barra de buckets

--- frontend/src/main_components/bucket_selector copy.js
+++ frontend/src/main_components/bucket_selector copy.js
@@ -1,26 +1,13 @@
-import { useState, useEffect } from 'react'
 import Bucket from '../components/bucket'
 
 function BucketSelector({buckets_list, set_current_bucket, is_loading}){
-    function handle_hidde_sidbar(){
-        const sidbar = document.getElementById('buckets-sidbar')
-        if (sidbar.classList.contains('sidbar-hidden')){
-            sidbar.classList.remove('sidbar-hidden')
-        } else {
-            sidbar.classList.add('sidbar-hidden')
-            // .sidbar-show
-        }
-    }
-
     return (
-        <>
-        <span id="sidbar-hidden-span" onClick={()=>{handle_hidde_sidbar()}}/>
-        <div id="buckets-sidbar" className="sidbar-show d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
+        <div id="buckets-sidbar" className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
             <div className="d-flex align-items-center mb-3 ms-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <span className="fs-4">Buckets</span>
             </div>
             
-            <ul className="nav nav-pills flex-column mb-auto" style={{flexWrap: "unset"}}>
+            <ul className="nav nav-pills flex-column mb-auto">
                 {
                     buckets_list.map(function(e){
                         return(
@@ -30,7 +17,6 @@ function BucketSelector({buckets_list, set_current_bucket, is_loading}){
                 }
             </ul>
         </div>
-        </>
     )
 }
 
