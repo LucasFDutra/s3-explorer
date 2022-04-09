b95b10e91dd8deae212fb13d234d0c436f2abb83
LucasFDutra <lucasfelipedutra@gmail.com>
2022-04-09 12:05:59 -0300

    removi o comando com alias

--- simulated_env/create_structure.sh
+++ simulated_env/create_structure.sh
@@ -1,3 +1,3 @@
-awslocal s3 mb s3://mybucket-1
-awslocal s3 mb s3://mybucket-2
-awslocal s3 cp conteudo-1 s3://mybucket-1/ --recursive
+aws --endpoint-url=http://localhost:4566 s3 mb s3://mybucket-1
+aws --endpoint-url=http://localhost:4566 s3 mb s3://mybucket-2
+aws --endpoint-url=http://localhost:4566 s3 cp conteudo-1 s3://mybucket-1/ --recursive
